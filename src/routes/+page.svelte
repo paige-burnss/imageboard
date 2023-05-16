5b15d3a7291b3d5f957c46da141685253d2224fb
Mimmzzyy <31726576+Mimmzzyy@users.noreply.github.com>
2023-05-16 06:12:19 +0200

    button alignemnt in appbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 
 import { AppBar } from '@skeletonlabs/skeleton';
-import { SlideToggle } from '@skeletonlabs/skeleton';
+import { SlideToggle, FileButton} from '@skeletonlabs/skeleton';
+
 
 let value: boolean = false;
 
 </script>
-
-<AppBar>
-    <svelte:fragment slot="trail"><SlideToggle name="slider-label" checked> <icon> dark mode</icon></SlideToggle> 
-</svelte:fragment>
+<AppBar class="items-end">
+<div class="flex flex-row-reverse items-center gap-4">
+        <SlideToggle class="py-4" name="slider-label" checked active="bg-primary-500" size="sm"> <icon> dark mode</icon></SlideToggle> 
+        <FileButton class="py-4" name="files" button="variant-soft-primary">Language</FileButton>
+    </div>
 </AppBar>
-
 <!-- <nav> <a href="/"> Home </a> 
     <a href="/boards"> Popular boards</a>
 
@@ -22,9 +23,20 @@ let value: boolean = false;
 
     <h1 class="text-center"> IMOSHI </h1>
 
+    <div class="card">
+        <header class="card-header text-center">Welcome to IMOSHI</header>
+        <section class="p-4 text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </section>
+    </div>
+
     
 </div>
 
+
+<div class="container">
+    <h3> How it works </h3>
+</div>
 
 
 <div class="container mx-auto">
